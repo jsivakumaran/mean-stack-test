955da26638a50955ad0a9bdf3f5e09ed00ea2f46
jsivakumaran <jsivakumaran@outlook.com>
2016-01-18 16:37:22 -0800

    Add initial repository based off of the angular-fullstack yeoman generator

---
+++ client/app/app.js
@@ -0,0 +1,20 @@
+'use strict';
+
+angular.module('storeApp', [
+  'storeApp.auth',
+  'storeApp.admin',
+  'storeApp.constants',
+  'ngCookies',
+  'ngResource',
+  'ngSanitize',
+  'btford.socket-io',
+  'ui.router',
+  'ui.bootstrap',
+  'validation.match'
+])
+  .config(function($urlRouterProvider, $locationProvider) {
+    $urlRouterProvider
+      .otherwise('/');
+
+    $locationProvider.html5Mode(true);
+  });
---
+++ client/components/oauth-buttons/oauth-buttons.controller.js
@@ -0,0 +1,8 @@
+'use strict';
+
+angular.module('storeApp')
+  .controller('OauthButtonsCtrl', function($window) {
+    this.loginOauth = function(provider) {
+      $window.location.href = '/auth/' + provider;
+    };
+  });
